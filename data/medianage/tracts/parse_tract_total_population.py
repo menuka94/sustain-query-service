a3e43a03b75b4362814e79890be38cf6a6b117c9
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-05-12 10:08:06 -0600

    add medianage - tract

---
+++ data/medianage/tracts/parse_tract_total_population.py
@@ -0,0 +1,26 @@
+from csv import reader
+
+file = open('acs5_2011_medianage_tracts.csv')
+
+lines = []
+
+for line in reader(file):
+    lines.append(line)
+
+# drop headings
+lines = lines[1:]
+
+table_name = "2011_tract_medianage"
+
+sql = ""
+
+for line in lines:
+    if float(line[2]) > 0:
+        sql += "INSERT INTO " + table_name + " VALUES (" + line[0] + "," + line[2] + ");\n"
+    else:
+        print("Negative value")
+
+outputFile = open('insert_statements.sql', 'w')
+outputFile.write(sql)
+outputFile.flush()
+outputFile.close()
