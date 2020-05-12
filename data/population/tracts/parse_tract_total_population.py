66c60b466822aae12a905ffb971e56e482981f22
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-05-11 22:28:29 -0600

    add 2011_tract_population

---
+++ data/population/tracts/parse_tract_total_population.py
@@ -0,0 +1,23 @@
+from csv import reader
+
+file = open('acs5_2011_population_tracts.csv')
+
+lines = []
+
+for line in reader(file):
+    lines.append(line)
+
+# drop headings
+lines = lines[1:]
+
+table_name = "2011_tract_total_population"
+
+sql = ""
+
+for line in lines:
+    sql += "INSERT INTO " + table_name + " VALUES (" + line[0] + ",'" + line[1] + "'," + line[2] + ");\n"
+
+outputFile = open('insert_statements.sql', 'w')
+outputFile.write(sql)
+outputFile.flush()
+outputFile.close()
