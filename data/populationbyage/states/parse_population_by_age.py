2e340e4e179abfac8645f3ec709e4bf389244f9c
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-05-19 02:08:40 -0600

    population-by-age: states - complete parser

--- data/populationbyage/states/parse_population_by_age.py
+++ data/populationbyage/states/parse_population_by_age.py
@@ -16,7 +16,7 @@ table_name = "2011_state_population_by_age"
 sql = ""
 
 for line in lines:
-    sql += "INSERT INTO " + table_name + " VALUES (" + line[0] + ",'" + line[1] + "'," \
+    sql += "INSERT INTO " + table_name + " VALUES (" + line[0] + ",'" + line[1] + "'," +  line[2] + "," \
         + line[3] + ","+ line[4] +"," + line[5] + "," + line[6] + "," + line[7] + "," \
         + line[8] + ","+ line[9] +"," + line[10] + "," + line[11] + "," + line[12] + "," \
         + line[13] + ","+ line[14] +"," + line[15] + "," + line[16] + "," + line[17] + "," \
@@ -24,9 +24,9 @@ for line in lines:
         + line[23] + ","+ line[24] +"," + line[25] + "," + line[26] + "," + line[27] + "," \
         + line[28] + ","+ line[29] +"," + line[30] + "," + line[31] + "," + line[32] + "," \
         + line[33] + ","+ line[34] +"," + line[35] + "," + line[36] + "," + line[37] + "," \
-        + line[38] + ","+ line[39] +"," + line[40] + "," + line[42] + "," + line[43] + "," \
-        + line[43] + ","+ line[44] +"," + line[45] + "," + line[47] + "," + line[48] + "," \
-        + line[49]  \
+        + line[38] + ","+ line[39] +"," + line[40] + "," + line[41] + "," + line[42] + "," \
+        + line[43] + ","+ line[44] +"," + line[45] + "," + line[46] + "," + line[47] + "," \
+        + line[48] + "," + line[49] \
             + ");\n"
 
 outputFile = open('insert_statements.sql', 'w')
