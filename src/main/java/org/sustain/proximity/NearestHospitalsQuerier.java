2925df56b583cdde7369ef8bf4158a0ba2aa6ee4
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2020-10-18 20:36:41 -0600

    calculate nearest hospitals to tract centroids

--- src/main/java/org/sustain/proximity/NearestHospitalsQuerier.java
+++ src/main/java/org/sustain/proximity/NearestHospitalsQuerier.java
@@ -1,11 +1,14 @@
 package org.sustain.proximity;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.geojson.Point;
 import com.mongodb.client.model.geojson.Position;
 import org.bson.Document;
@@ -13,7 +16,11 @@ import org.sustain.DatasetRequest;
 import org.sustain.db.mongodb.DBConnection;
 import org.sustain.util.Constants;
 
-import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class NearestHospitalsQuerier {
@@ -24,39 +31,97 @@ public class NearestHospitalsQuerier {
                 db.getCollection(Constants.DATASETS.get(DatasetRequest.Dataset.HOSPITALS));
         FindIterable<Document> documents = centers.find();
 
-        HashMap<String, ArrayList<String>> nearestHospitals = new HashMap<>();
+        HashMap<String, HashMap<String, Double>> nearestHospitalsMap = new HashMap<>();
+        int i = 0;
         // for each center
         for (Document document : documents) {
+            i++;
+            if (i % 1000 == 0) {
+                System.out.println(i);
+            }
             double x = document.getDouble("x");
             double y = document.getDouble("y");
             String gisjoin = document.getString("GISJOIN");
-            ArrayList<String> hospitalIds = new ArrayList<>();
+            HashMap<String, Double> hospitalDistanceMap = new HashMap<>();
 
-            System.out.println("GISJOIN: " + gisjoin);
+            //System.out.println("GISJOIN: " + gisjoin);
 
             Point refPoint = new Point(new Position(x, y));
 
+            /**
+             * db.new_stores.aggregate([
+             *     { "$geoNear": {
+             *         "near": {
+             *             "type": "Point",
+             *             "coordinates": [ -81.093699, 32.074673 ]
+             *         },
+             *         "maxDistance": 500 * 1609,
+             *         "spherical": true,
+             *         "distanceField": "distance",
+             *         "distanceMultiplier": 0.000621371
+             *     }}
+             * ]).pretty()
+             */
             double maxDistance = 1000.0;
             int hospitalsFound = 0;
+
+
             while (hospitalsFound == 0) {
-                FindIterable<Document> hospitalsNearby = hospitals.find(Filters.near("geometry", refPoint, maxDistance,
-                        0.0));
+                BasicDBObject dbObject = new BasicDBObject("$geoNear",
+                        new BasicDBObject("near", new BasicDBObject("type", "Point")
+                                .append("coordinates", new double[]{x, y}))
+                                .append("maxDistance", maxDistance)
+                                .append("spherical", true)
+                                .append("distanceField", "distance")
+                );
+                AggregateIterable<Document> hospitalsNearby = hospitals.aggregate(Collections.singletonList(dbObject));
+
                 MongoCursor<Document> cursor = hospitalsNearby.cursor();
                 while (cursor.hasNext()) {
                     Document next = cursor.next();
                     String hospitalId =
                             JsonParser.parseString(next.toJson()).getAsJsonObject().get("properties").getAsJsonObject().get("ID").getAsString();
-                    hospitalIds.add(hospitalId);
-                    System.out.println("Hospital ID: " + hospitalId);
+                    double distance =
+                            JsonParser.parseString(next.toJson()).getAsJsonObject().get("distance").getAsDouble();
+                    //System.out.println("Hospital ID: " + hospitalId);
+                    //System.out.println("Distance: " + distance);
+
+                    hospitalDistanceMap.put(hospitalId, distance);
                     hospitalsFound++;
                 }
                 maxDistance += 1000;
-                //System.out.println("Increasing maxDistance: " + maxDistance);
             }
 
-            System.out.println("hospitalsFound: " + hospitalsFound);
-            nearestHospitals.put(gisjoin, hospitalIds);
-            System.out.println();
+            //System.out.println("hospitalsFound: " + hospitalsFound);
+            nearestHospitalsMap.put(gisjoin, hospitalDistanceMap);
+            //System.out.println();
         }
+
+        System.out.println("Results");
+        JsonArray finalArray = new JsonArray();
+        for (String gisJoin : nearestHospitalsMap.keySet()) {
+            JsonObject entry = new JsonObject();
+            entry.addProperty("GISJOIN", gisJoin);
+
+            JsonObject nearestHospitals = new JsonObject();
+            HashMap<String, Double> hospitalDistanceMap = nearestHospitalsMap.get(gisJoin);
+            for (String hospitalId : hospitalDistanceMap.keySet()) {
+                nearestHospitals.addProperty(hospitalId, hospitalDistanceMap.get(hospitalId));
+            }
+
+            entry.add("nearest_hospitals", nearestHospitals);
+            //System.out.println(entry.toString());
+            //System.out.println();
+            finalArray.add(entry);
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("tract_nearest_hospitals.json")));
+            writer.write(finalArray.toString());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
