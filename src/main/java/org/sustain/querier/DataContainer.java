caa36e36eb78aadbe334e63e8a2247eabf214be8
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-01-04 19:38:39 -0700

    temporary fix for single queries
    ignore GISJOIN field if it does not exist

--- src/main/java/org/sustain/querier/DataContainer.java
+++ src/main/java/org/sustain/querier/DataContainer.java
@@ -1,65 +1,65 @@
 package org.sustain.querier;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import io.grpc.stub.StreamObserver;
+import org.json.JSONObject;
+import org.sustain.CompoundResponse;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.google.gson.JsonParser;
-import org.json.JSONObject;
-import com.google.gson.Gson;
-
-import org.sustain.CompoundResponse;
-import org.sustain.CompoundRequest;
-
-import io.grpc.stub.StreamObserver;
 
 public class DataContainer {
-	private HashMap<String,HashMap<String,String>> documents;
+    private HashMap<String, HashMap<String, String>> documents;
 
-	public DataContainer() {
-		this.documents = new HashMap<String,HashMap<String,String>>();
+    public DataContainer() {
+        this.documents = new HashMap<String, HashMap<String, String>>();
     }
 
-	public DataContainer(HashMap<String,HashMap<String,String>> documents) {
-		this.documents = documents;
+    public DataContainer(HashMap<String, HashMap<String, String>> documents) {
+        this.documents = documents;
     }
 
-	public HashMap<String,HashMap<String,String>> getDocuments(){
-		return documents;
-	}
+    public HashMap<String, HashMap<String, String>> getDocuments() {
+        return documents;
+    }
 
-	public void addData(String datum){
-		String gisjoin = JsonParser.parseString(datum).getAsJsonObject().get("GISJOIN").toString().replace("\"", "");
-		documents.put(gisjoin, new Gson().fromJson(datum, HashMap.class));
-	}
+    public void addData(String datum) {
+        if (JsonParser.parseString(datum).getAsJsonObject().has("GISJOIN")) {
+            String gisjoin = JsonParser.parseString(datum).getAsJsonObject().get("GISJOIN").toString().replace("\"",
+                    "");
+            documents.put(gisjoin, new Gson().fromJson(datum, HashMap.class));
+        } else {
+            documents.put("", new Gson().fromJson(datum, HashMap.class));
+        }
+    }
 
-	public DataContainer innerJoin(DataContainer other){
-		//documents.entrySet().forEach(entry->{
-		//	System.out.println(entry.getKey() + " " + entry.getValue());  
-		//});
+    public DataContainer innerJoin(DataContainer other) {
+        //documents.entrySet().forEach(entry->{
+        //	System.out.println(entry.getKey() + " " + entry.getValue());
+        //});
 
-		HashMap<String,HashMap<String,String>> results = new HashMap<String,HashMap<String,String>>();
+        HashMap<String, HashMap<String, String>> results = new HashMap<String, HashMap<String, String>>();
 
-		for (Map.Entry<String, HashMap<String,String>> entry : other.getDocuments().entrySet()) {
-			String key = entry.getKey();
-                        //System.out.println(key);
-			HashMap<String,String> value = entry.getValue();
+        for (Map.Entry<String, HashMap<String, String>> entry : other.getDocuments().entrySet()) {
+            String key = entry.getKey();
+            //System.out.println(key);
+            HashMap<String, String> value = entry.getValue();
 
-			// Inner join - we only want GISJOINS in both queries
-			if (documents.containsKey(key)){
-                                //System.out.println(key);
-				value.putAll(documents.get(key));
-				results.put(key, value);
-			}
-		}
-		
-		return new DataContainer(results);
-	}
+            // Inner join - we only want GISJOINS in both queries
+            if (documents.containsKey(key)) {
+                //System.out.println(key);
+                value.putAll(documents.get(key));
+                results.put(key, value);
+            }
+        }
 
-	public void writeToClient(StreamObserver<CompoundResponse> responseObserver){
-		for (HashMap<String,String> value : documents.values()) {
-			responseObserver.onNext(CompoundResponse.newBuilder().setData(new JSONObject(value).toString()).setGeoJson("").build());
-		}
-	}
+        return new DataContainer(results);
+    }
+
+    public void writeToClient(StreamObserver<CompoundResponse> responseObserver) {
+        for (HashMap<String, String> value : documents.values()) {
+            responseObserver.onNext(CompoundResponse.newBuilder().setData(new JSONObject(value).toString()).setGeoJson("").build());
+        }
+    }
 }
