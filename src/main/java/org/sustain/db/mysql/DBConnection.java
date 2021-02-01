f7e63ed84b02d20734ea1b90d49fe4a5d13e077e
Caleb Carlson <ccarlson355@gmail.com>
2021-02-01 15:57:02 -0700

    feature(devops): Support environment variables in core code
    * Use System.getenv() for configuration vars
    * Remove Utils.java and related config file parsing functions
    * Remove config.properties
    * Log environment variables at beginning of SustainServer/SpatialClient for testing/debug purposes.

---
+++ src/main/java/org/sustain/db/mysql/DBConnection.java
@@ -0,0 +1,51 @@
+package org.sustain.db.mysql;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sustain.util.Constants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final Logger log = LogManager.getLogger(DBConnection.class);
+    private static final String driverName = "com.mysql.cj.jdbc.Driver";
+
+    private static String username;
+    private static String password;
+    private static String host;
+
+    private static void initDatabaseProperties() {
+        username  = Constants.DB.USERNAME;
+        password  = Constants.DB.PASSWORD;
+        host      = Constants.DB.HOST;
+    }
+
+    public static Connection getConnection(String dbName) {
+        log.debug("DBName: " + dbName);
+        initDatabaseProperties();
+        String url = "jdbc:mysql://" + host + ":3306/" + dbName + "?useSSL=false&" +
+                "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        log.debug("Connection URL: " + url);
+        Connection con = null;
+        try {
+            Class.forName(driverName);
+            try {
+                con = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                log.info("Failed to create the database connection.");
+                log.info("Exiting ...");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("Driver not found.");
+            log.info("Exiting ...");
+            System.exit(1);
+        }
+        return con;
+    }
+}
+
