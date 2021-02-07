9678e3b52f777a256e693b40e7fc1b76425faa4d
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-02-07 02:10:11 -0700

    feat: add 'total_population' to county_stats

--- src/main/java/org/sustain/proximity/SuperCollectionCreator.java
+++ src/main/java/org/sustain/proximity/SuperCollectionCreator.java
@@ -1,9 +1,12 @@
 package org.sustain.proximity;
 
 import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
@@ -16,19 +19,35 @@ import java.util.ArrayList;
 public class SuperCollectionCreator {
     private static final Logger log = LogManager.getLogger(SuperCollectionCreator.class);
 
+    private static final ArrayList<String> gisJoins = new ArrayList<>();
+
     public static void main(String[] args) {
+        getGisJoinsList();
+        MongoDatabase db = DBConnection.getConnection();
+        MongoCollection<Document> countyStats = db.getCollection("county_stats");
+
+        MongoCollection<Document> data = db.getCollection("county_total_population");
+        for (String gisJoin : gisJoins) {
+            BasicDBObject whereQuery = new BasicDBObject();
+            whereQuery.put("GISJOIN", gisJoin);
+            FindIterable<Document> documents = data.find(whereQuery);
+            Document field = documents.first();
+            int population = Integer.parseInt(field.get("2010_total_population").toString());
+            countyStats.updateOne(Filters.eq("GISJOIN", gisJoin), Updates.set("total_population", population));
+        }
+    }
+
+    private static void getGisJoinsList() {
         MongoDatabase db = DBConnection.getConnection();
         MongoCollection<Document> counties = db.getCollection(Constants.GeoJsonCollections.COUNTIES_GEO);
         FindIterable<Document> documents = counties.find();
 
-        ArrayList<String> gisJoins = new ArrayList<>();
+        Gson gson = new Gson();
         for (Document document : documents) {
-            Gson gson = new Gson();
             GeoJson county = gson.fromJson(document.toJson(), GeoJson.class);
             String gisJoin = county.getProperties().getGisJoin();
             gisJoins.add(gisJoin);
         }
 
-        log.info("Count: " + gisJoins.size());
     }
 }
