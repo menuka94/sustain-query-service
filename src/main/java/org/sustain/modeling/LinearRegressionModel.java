e265e99c396b4cb9b9ee61b269d26fafcadc93a2
Caleb Carlson <ccarlson355@gmail.com>
2021-02-11 18:51:38 -0700

    feature(core): Remove unused import statements

--- src/main/java/org/sustain/modeling/LinearRegressionModel.java
+++ src/main/java/org/sustain/modeling/LinearRegressionModel.java
@@ -130,23 +130,13 @@
 
 package org.sustain.modeling;
 
-import com.mongodb.Function;
+
 import com.mongodb.spark.MongoSpark;
-import com.mongodb.QueryBuilder;
-import com.mongodb.DBObject;
 import com.mongodb.spark.config.ReadConfig;
-import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FilterFunction;
-import org.apache.spark.ml.linalg.VectorUDT;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 import org.bson.Document;
 import scala.collection.JavaConverters;
 
