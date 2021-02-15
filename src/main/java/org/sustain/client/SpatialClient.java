4f8a731a10100a4e652dda610549c56708c4f25d
Caleb Carlson <ccarlson355@gmail.com>
2021-02-15 08:42:51 -0700

    feature(core): Log environment vars on startup

--- src/main/java/org/sustain/client/SpatialClient.java
+++ src/main/java/org/sustain/client/SpatialClient.java
@@ -35,6 +35,7 @@ public class SpatialClient {
     }
 
     public static void main(String[] args) {
+        logEnvironment();
         SustainGrpc.SustainBlockingStub sustainBlockingStub = new SpatialClient().getSustainBlockingStub();
 
         //exampleSpatialQuery(sustainBlockingStub, geoJson);
@@ -45,6 +46,20 @@ public class SpatialClient {
         //        SampleGeoJson.COLORADO);
         //exampleSviQuery(SampleGeoJson.COLORADO, SpatialOp.GeoIntersects, sustainBlockingStub);
     }
+
+    // Logs the environment variables that the server was started with.
+    public static void logEnvironment() {
+        log.info("--- Server Environment ---");
+        log.info("SERVER_HOST: " + Constants.Server.HOST);
+        log.info("SERVER_PORT: " + Constants.Server.PORT);
+        log.info("--- Database Environment ---");
+        log.info("DB_HOST: " + Constants.DB.HOST);
+        log.info("DB_PORT: " + Constants.DB.PORT);
+        log.info("DB_NAME: " + Constants.DB.NAME);
+        log.info("DB_USERNAME: " + Constants.DB.USERNAME);
+        log.info("DB_PASSWORD: " + Constants.DB.PASSWORD);
+    }
+
 
     private static void exampleDatasetQuery(DatasetRequest.Dataset dataset,
                                             SustainGrpc.SustainBlockingStub sustainBlockingStub, String geoJson) {
--- src/main/java/org/sustain/server/SustainServer.java
+++ src/main/java/org/sustain/server/SustainServer.java
@@ -51,9 +51,23 @@ public class SustainServer {
     private Server server;
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        logEnvironment();
         final SustainServer server = new SustainServer();
         server.start();
         server.blockUntilShutdown();
+    }
+
+    // Logs the environment variables that the server was started with.
+    public static void logEnvironment() {
+        log.info("--- Server Environment ---");
+        log.info("SERVER_HOST: " + Constants.Server.HOST);
+        log.info("SERVER_PORT: " + Constants.Server.PORT);
+        log.info("--- Database Environment ---");
+        log.info("DB_HOST: " + Constants.DB.HOST);
+        log.info("DB_PORT: " + Constants.DB.PORT);
+        log.info("DB_NAME: " + Constants.DB.NAME);
+        log.info("DB_USERNAME: " + Constants.DB.USERNAME);
+        log.info("DB_PASSWORD: " + Constants.DB.PASSWORD);
     }
 
 
