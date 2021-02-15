5e72f1ab8e288bd953c52ca9d89e5a0115cf2f30
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-02-14 18:56:06 -0700

    feat: add 'median_household_income' to county_stats

--- src/main/java/org/sustain/proximity/SuperCollectionCreator.java
+++ src/main/java/org/sustain/proximity/SuperCollectionCreator.java
@@ -1,7 +1,6 @@
 package org.sustain.proximity;
 
 import com.google.gson.Gson;
-import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -26,14 +25,16 @@ public class SuperCollectionCreator {
         MongoDatabase db = DBConnection.getConnection();
         MongoCollection<Document> countyStats = db.getCollection("county_stats");
 
-        MongoCollection<Document> data = db.getCollection("county_total_population");
+        MongoCollection<Document> data = db.getCollection("county_median_household_income");
         for (String gisJoin : gisJoins) {
-            BasicDBObject whereQuery = new BasicDBObject();
-            whereQuery.put("GISJOIN", gisJoin);
-            FindIterable<Document> documents = data.find(whereQuery);
+            FindIterable<Document> documents = data.find(Filters.eq("GISJOIN", gisJoin));
             Document field = documents.first();
-            int population = Integer.parseInt(field.get("2010_total_population").toString());
-            countyStats.updateOne(Filters.eq("GISJOIN", gisJoin), Updates.set("total_population", population));
+            if (field != null) {
+                double population = Double.parseDouble(field.get("2010_median_household_income").toString());
+                countyStats.updateOne(Filters.eq("GISJOIN", gisJoin), Updates.set("median_household_income", population));
+            } else {
+                log.info("No data");
+            }
         }
     }
 
