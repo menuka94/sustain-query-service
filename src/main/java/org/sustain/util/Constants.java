5a698fe204c1d0798480ac1f314ff0f4ea08524a
Caleb Carlson <ccarlson355@gmail.com>
2021-03-04 14:50:13 -0700

    refactor(core): Formatting, removal of dead code

--- src/main/java/org/sustain/util/Constants.java
+++ src/main/java/org/sustain/util/Constants.java
@@ -5,7 +5,6 @@ import org.sustain.ComparisonOperator;
 import org.sustain.DatasetRequest;
 import org.sustain.Decade;
 import org.sustain.OsmRequest;
-import org.sustain.Predicate;
 
 import java.util.HashMap;
 
@@ -19,6 +18,7 @@ public class Constants {
         put(Decade._1990, "1990");
         put(Decade._1980, "1980");
     }};
+
     public static final HashMap<CensusResolution, String> TARGET_RESOLUTIONS =
             new HashMap<CensusResolution, String>() {{
                 put(CensusResolution.State, CensusResolutions.STATE);
@@ -26,6 +26,7 @@ public class Constants {
                 put(CensusResolution.Tract, CensusResolutions.TRACT);
                 put(CensusResolution.Block, CensusResolutions.BLOCK);
             }};
+
     public static final HashMap<ComparisonOperator, String> COMPARISON_OPS =
             new HashMap<ComparisonOperator, String>() {{
                 put(ComparisonOperator.EQUAL, "=");
@@ -34,6 +35,7 @@ public class Constants {
                 put(ComparisonOperator.LESS_THAN, "<");
                 put(ComparisonOperator.LESS_THAN_OR_EQUAL, "<=");
             }};
+
     public static final HashMap<OsmRequest.Dataset, String> OSM_DATASETS =
             new HashMap<OsmRequest.Dataset, String>() {{
                 put(OsmRequest.Dataset.POINTS, "osm_points_geo");
@@ -42,6 +44,7 @@ public class Constants {
                 put(OsmRequest.Dataset.MULTI_POLYGONS, "osm_multipolygons_geo");
                 put(OsmRequest.Dataset.OTHER, "osm_other_geo");
             }};
+
     public static final HashMap<DatasetRequest.Dataset, String> DATASETS =
             new HashMap<DatasetRequest.Dataset, String>() {{
                 put(DatasetRequest.Dataset.DAMS, "dams_geo");
@@ -60,9 +63,9 @@ public class Constants {
     }
 
     public static class GeoJsonCollections {
-        public static final String STATES_GEO = "state_geo";
+        public static final String STATES_GEO   = "state_geo";
         public static final String COUNTIES_GEO = "county_geo";
-        public static final String TRACTS_GEO = "tract_geo";
+        public static final String TRACTS_GEO   = "tract_geo";
     }
 
     public static class DB {
@@ -74,19 +77,19 @@ public class Constants {
     }
 
     public static class CensusFeatures {
-        public static final String TOTAL_POPULATION = "total_population";
+        public static final String TOTAL_POPULATION        = "total_population";
         public static final String MEDIAN_HOUSEHOLD_INCOME = "median_household_income";
-        public static final String POPULATION_BY_AGE = "population_by_age";
-        public static final String MEDIAN_AGE = "medianage";
-        public static final String POVERTY = "poverty";
-        public static final String RACE = "race";
+        public static final String POPULATION_BY_AGE       = "population_by_age";
+        public static final String MEDIAN_AGE              = "medianage";
+        public static final String POVERTY                 = "poverty";
+        public static final String RACE                    = "race";
     }
 
     public static class CensusResolutions {
-        public static final String STATE = "state";
+        public static final String STATE  = "state";
         public static final String COUNTY = "county";
-        public static final String TRACT = "tract";
-        public static final String BLOCK = "block";
+        public static final String TRACT  = "tract";
+        public static final String BLOCK  = "block";
     }
 
     public static class Spark {
