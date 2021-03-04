565aa376ed9215d654827b5c6ebeb1e43f2a36c1
Caleb Carlson <ccarlson355@gmail.com>
2021-03-04 15:00:30 -0700

    refactor(core): Fix iterative logging

--- src/main/java/org/sustain/handlers/RegressionQueryHandler.java
+++ src/main/java/org/sustain/handlers/RegressionQueryHandler.java
@@ -23,38 +23,7 @@ public class RegressionQueryHandler extends GrpcHandler<ModelRequest, ModelRespo
 
     @Override
     void logRequest(ModelRequest request) {
-        log.info("============== REQUEST ===============");
-
-        log.info("Collections:");
-        for (int i = 0; i < request.getCollectionsCount(); i++) {
-            Collection col = request.getCollections(i);
-            log.info("\tName: {}", col.getName());
-            log.info("\tLabel: {}", col.getLabel());
-            log.info("\tFeatures:");
-            for (int j = 0; j < col.getFeaturesCount(); j++) {
-                log.info("\t\t{}", col.getFeatures(j));
-            }
-        }
-
-        log.info("LinearRegressionRequest:");
-        LinearRegressionRequest req = request.getLinearRegressionRequest();
-        log.info("\tGISJoins:");
-        for (int i = 0; i < req.getGisJoinsCount(); i++) {
-            log.info("\t\t{}", req.getGisJoins(i));
-        }
-
-        log.info("\tLoss: {}", req.getLoss());
-        log.info("\tSolver: {}", req.getSolver());
-        log.info("\tMaxIterations: {}", req.getMaxIterations());
-        log.info("\tAggregationDepth: {}", req.getAggregationDepth());
-        log.info("\tElasticNetParam: {}", req.getElasticNetParam());
-        log.info("\tEpsilon: {}", req.getEpsilon());
-        log.info("\tEpsilon: {}", req.getEpsilon());
-        log.info("\tConvergenceTolerance: {}", req.getConvergenceTolerance());
-        log.info("\tRegularizationParam: {}", req.getRegularizationParam());
-        log.info("\tSetStandardization: {}", req.getSetStandardization());
-        log.info("\tFitIntercept: {}", req.getFitIntercept());
-        log.info("=======================================");
+        log.info("\n--- ModelRequest ---\n{}", request.toString());
     }
 
     @Override
