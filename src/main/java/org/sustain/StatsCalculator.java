dca618bcf4ef99d24c9a7fef75fa904e51715fef
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-03-04 23:03:48 -0700

    feat: update StatsCalculator

--- src/main/java/org/sustain/StatsCalculator.java
+++ src/main/java/org/sustain/StatsCalculator.java
@@ -3,6 +3,7 @@ package org.sustain;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.sustain.db.mongodb.DBConnection;
 
@@ -14,10 +15,30 @@ public class StatsCalculator {
         MongoDatabase db = DBConnection.getConnection();
         MongoCollection<Document> counties = db.getCollection("county_geo");
 
+        String infraCollectionName = "dams";
+        MongoCollection<Document> infraCollection = db.getCollection(infraCollectionName + "_geo");
+        String resolution = "county";
+
+        String statsCollectionName = resolution + "_stats";
+        MongoCollection<Document> stats = db.getCollection(statsCollectionName);
+
+        int totalInfraEntities = 0;
         FindIterable<Document> documents = counties.find();
         for (Document document : documents) {
             String gisJoin = document.getString("GISJOIN");
-            System.out.println(gisJoin);
+            FindIterable<Document> matches = infraCollection.find(Filters.eq("GISJOIN_" + resolution, gisJoin));
+            int infraEntityCount = 0;
+            for (Document match : matches) {
+                infraEntityCount++;
+            }
+            totalInfraEntities += infraEntityCount;
+
+            stats.updateOne(Filters.eq("GISJOIN", gisJoin), new Document("$set", new Document(
+                    "no_of_" + infraCollectionName, infraEntityCount
+            )));
+            System.out.println(gisJoin + ": " + infraEntityCount);
+
         }
+        System.out.println("Total " + infraCollectionName + ": " + totalInfraEntities);
     }
 }
