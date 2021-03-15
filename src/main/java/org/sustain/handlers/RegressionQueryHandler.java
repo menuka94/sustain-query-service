1c944b8a2b47225b5636aad06f0a6337cac7a5ac
Caleb Carlson <ccarlson355@gmail.com>
2021-03-15 10:39:02 -0600

    feature(core): Set spark config at runtime

--- src/main/java/org/sustain/handlers/RegressionQueryHandler.java
+++ src/main/java/org/sustain/handlers/RegressionQueryHandler.java
@@ -39,9 +39,17 @@ public class RegressionQueryHandler extends ModelHandler {
             LinearRegressionRequest lrRequest = this.request.getLinearRegressionRequest();
             Collection requestCollection = this.request.getCollections(0); // We only support 1 collection currently
 
+            String mongoUri = String.format("mongodb://%s:%s", Constants.DB.HOST, Constants.DB.PORT);
+
+
             // Create a custom ReadConfig
             Map<String, String> readOverrides = new HashMap<String, String>();
-            readOverrides.put("uri", String.format("mongodb://%s:%s", Constants.DB.HOST, Constants.DB.PORT));
+            this.sparkContext.getConf()
+                    .set("spark.mongodb.input.uri", mongoUri)
+                    .set("spark.mongodb.input.database", Constants.DB.NAME)
+                    .set("spark.mongodb.input.collection", requestCollection.getName());
+
+            readOverrides.put("uri", mongoUri);
             readOverrides.put("database", Constants.DB.NAME);
             readOverrides.put("collection", requestCollection.getName());
             ReadConfig readConfig = ReadConfig.create(this.sparkContext).withOptions(readOverrides);
