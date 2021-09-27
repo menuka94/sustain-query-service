8f7f060d0315aa799e22ec7214f3a6d9855308e5
psmith36 <psmith36@rams.colostate.edu>
2021-09-27 14:24:45 -0600

    feature(core): add HTTP requests to Druid handler

--- src/main/java/org/sustain/handlers/DruidDirectQueryHandler.java
+++ src/main/java/org/sustain/handlers/DruidDirectQueryHandler.java
@@ -6,8 +6,14 @@ import org.apache.logging.log4j.Logger;
 import org.sustain.DruidDirectRequest;
 import org.sustain.DruidDirectResponse;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class DruidDirectQueryHandler extends GrpcHandler<DruidDirectRequest, DruidDirectResponse> {
     public static final Logger log = LogManager.getLogger(DruidDirectQueryHandler.class);
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
     public DruidDirectQueryHandler(DruidDirectRequest request, StreamObserver<DruidDirectResponse> responseObserver) {
         super(request, responseObserver);
@@ -18,10 +24,31 @@ public class DruidDirectQueryHandler extends GrpcHandler<DruidDirectRequest, Dru
         log.info("Received a Druid query");
 
         try {
+            HttpRequest druidRequest = HttpRequest.newBuilder()
+                .uri(new URI("http://lattice-123.cs.colostate.edu:8088/druid/v2"))
+                .POST(HttpRequest.BodyPublishers.ofString(request.getQuery()))
+                .build();
+
+            HttpResponse<String> druidResponse = httpClient.send(druidRequest, HttpResponse.BodyHandlers.ofString());
+
 
         } catch (Exception e) {
             log.error("Failed to evaluate query", e);
             responseObserver.onError(e);
+        }
+    }
+
+    private static class DruidStreamWriter extends StreamWriter<String, DruidDirectResponse> {
+        public DruidStreamWriter(
+            StreamObserver<DruidDirectResponse> responseObserver,
+            int threadCount
+        ) {
+            super(responseObserver, threadCount);
+        }
+
+        @Override
+        public DruidDirectResponse convert(String s) {
+            return DruidDirectResponse.newBuilder().setData(s).build();
         }
     }
 }
