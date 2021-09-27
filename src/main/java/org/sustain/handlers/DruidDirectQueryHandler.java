edd2672aefc0d66c0d39c3e6bdd99e366ce90248
psmith36 <psmith36@rams.colostate.edu>
2021-09-27 14:53:22 -0600

    feature(core): implement JSON deserialization

--- src/main/java/org/sustain/handlers/DruidDirectQueryHandler.java
+++ src/main/java/org/sustain/handlers/DruidDirectQueryHandler.java
@@ -3,6 +3,9 @@ package org.sustain.handlers;
 import io.grpc.stub.StreamObserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.sustain.DruidDirectRequest;
 import org.sustain.DruidDirectResponse;
 
@@ -29,16 +32,30 @@ public class DruidDirectQueryHandler extends GrpcHandler<DruidDirectRequest, Dru
                 .POST(HttpRequest.BodyPublishers.ofString(request.getQuery()))
                 .build();
 
+            // Druid provides responses in the form of JSON arrays.
             HttpResponse<String> druidResponse = httpClient.send(druidRequest, HttpResponse.BodyHandlers.ofString());
+            DruidStreamWriter writer = new DruidStreamWriter(responseObserver, 1);
+            JSONArray results = new JSONArray(druidResponse.body());
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject row;
+                if ((row = results.optJSONObject(i)) != null) {
+                    writer.add(row);
+                } else {
+                    log.warn("Encountered an unexpected (non-object) row in result set: {}", row);
+                }
+            }
 
 
+        } catch (JSONException e) {
+            log.error("Deserialization of Druid response failed", e);
+            responseObserver.onError(e);
         } catch (Exception e) {
             log.error("Failed to evaluate query", e);
             responseObserver.onError(e);
         }
     }
 
-    private static class DruidStreamWriter extends StreamWriter<String, DruidDirectResponse> {
+    private static class DruidStreamWriter extends StreamWriter<JSONObject, DruidDirectResponse> {
         public DruidStreamWriter(
             StreamObserver<DruidDirectResponse> responseObserver,
             int threadCount
@@ -47,8 +64,8 @@ public class DruidDirectQueryHandler extends GrpcHandler<DruidDirectRequest, Dru
         }
 
         @Override
-        public DruidDirectResponse convert(String s) {
-            return DruidDirectResponse.newBuilder().setData(s).build();
+        public DruidDirectResponse convert(JSONObject queryResultRow) {
+            return DruidDirectResponse.newBuilder().setData(queryResultRow.toString()).build();
         }
     }
 }
