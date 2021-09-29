654fb6678c93c54009b9ddfd06523e0cecd1ebc1
psmith36 <psmith36@rams.colostate.edu>
2021-09-29 15:21:20 -0600

    fix(core) fix bad serialization in test script

--- nodejs-client/druid_request.js
+++ nodejs-client/druid_request.js
@@ -17,31 +17,33 @@ let service = protoDescriptor.sustain;
 let stub = new service.Sustain('localhost:50051', grpc.credentials.createInsecure());
 
 let druidRequest = {
-    queryType: "groupBy",
-    dataSource: "Gridmet_ALL_Partitioned",
-    granularity: "year",
-    columns: [ "GISJOIN" ],
-    intervals: [ "1979-01-01/1999-01-01" ],
-    filter: {
-        type: "selector",
-        dimension: "GISJOIN",
-        value: "G4900450130600"
-    },
-    aggregations: [
-        {
-            type: "doubleMean",
-            name: "pa_mean",
-            fieldName: "precipitation_amount"
+    query: `{
+        "queryType": "groupBy",
+        "dataSource": "Gridmet_ALL_Partitioned",
+        "granularity": "year",
+        "columns": ["GISJOIN"],
+        "intervals": ["1979-01-01/1999-01-01"],
+        "filter": {
+            "type": "selector",
+            "dimension": "GISJOIN",
+            "value": "G4900450130600"
         },
-        {
-            type: "count",
-            name: "atm_count",
-            fieldName: "air_temperature_max"
-        }
-    ]
+        "aggregations": [
+            {
+                "type": "doubleMean",
+                "name": "pa_mean",
+                "fieldName": "precipitation_amount"
+            },
+            {
+                "type": "count",
+                "name": "atm_count",
+                "fieldName": "air_temperature_max"
+            }
+        ]
+    }`
 };
 
-let call = stub.DruidDirectQuery(JSON.stringify(druidRequest));
+let call = stub.DruidDirectQuery(druidRequest);
 
 call.on('data', function (response) {
     console.log(response.data, "\n");
